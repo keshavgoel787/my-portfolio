81e4de55372a93506ded38f025bf3e4cb432e534
Keshav Goel <141261924+keshavgoel787@users.noreply.github.com>
2025-07-16 19:58:34 -0400

    base experience

---
+++ src/pages/ExperiencePage.tsx
@@ -0,0 +1,10 @@
+// src/pages/ExperiencesPage.tsx
+import ConstellationScene from "../components/ConstellationScene";
+
+export default function ExperiencesPage() {
+  return (
+    <section style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
+      <ConstellationScene />
+    </section>
+  );
+}
